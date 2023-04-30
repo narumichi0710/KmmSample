132cc163c909a8c1c1dfed28119140efef5a26b9
narumichikubo <narumichi7373@gmail.com>
2023-04-30 16:58:31 +0900

    チュートリアル2, 3ステップのソースを追加

--- shared/src/commonMain/kotlin/com/example/kmmsample/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/kmmsample/Greeting.kt
@@ -1,9 +1,22 @@
 package com.example.kmmsample
 
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.daysUntil
+import kotlinx.datetime.todayIn
+
 class Greeting {
     private val platform: Platform = getPlatform()
 
     fun greet(): String {
-        return "Hello, ${platform.name}!"
+        return "Guess what it is! > ${platform.name.reversed()}!" +
+                "\nThere are only ${daysUntilNewYear()} days left until New Year! 🎆"
     }
+    private fun daysUntilNewYear(): Int {
+        val today = Clock.System.todayIn(TimeZone.currentSystemDefault())
+        val closestNewYear = LocalDate(today.year + 1, 1, 1)
+        return today.daysUntil(closestNewYear)
+    }
+
 }
