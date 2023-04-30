d61fcdb9c8b91ebcdfd9cc39cd24b72b4cd28677
narumichikubo <narumichi7373@gmail.com>
2023-04-30 18:02:33 +0900

    チュートリアル3のiOS側の実装を追加

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,11 +1,30 @@
 import SwiftUI
 import shared
 
+class SampleData: ObservableObject {
+    @Published var text = "Loading..."
+    
+    func fetch() {
+        Greeting().greet { greeting, error in
+           DispatchQueue.main.async {
+               if let greeting = greeting {
+                   self.text = greeting
+               } else {
+                   self.text = error?.localizedDescription ?? "error"
+               }
+           }
+        }
+    }
+}
+
 struct ContentView: View {
-	let greet = Greeting().greet()
+    @StateObject private(set) var data: SampleData = .init()
 
 	var body: some View {
-		Text(greet)
+        Text(data.text)
+            .onAppear {
+                data.fetch()
+            }
 	}
 }
 
