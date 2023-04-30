6b6e393bb61a18ba02c561641ddd60486a397e94
narumichikubo <narumichi7373@gmail.com>
2023-04-30 17:32:59 +0900

    チュートリアル3のAndroid側の実装を追加

--- androidApp/src/main/java/com/example/kmmsample/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/kmmsample/android/MainActivity.kt
@@ -6,6 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kmmsample.Greeting
@@ -19,13 +24,20 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    var text by remember { mutableStateOf("Loading") }
+                    LaunchedEffect(true) {
+                        text = try {
+                            Greeting().greet()
+                        } catch (e: Exception) {
+                            e.localizedMessage ?: "error"
+                        }
+                    }
+                    GreetingView(text)
                 }
             }
         }
     }
 }
-
 @Composable
 fun GreetingView(text: String) {
     Text(text = text)
