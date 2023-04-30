f5a3bd9bff1d85b3e44776e9dd8021d35b75247a
narumichikubo <narumichi7373@gmail.com>
2023-04-30 13:48:12 +0900

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KmmSample"
+include(":androidApp")
+include(":shared")
